c24b80ebe41b9bbb133014ee4ab44e99d1982949
Emanuele Ballarin <emanuele@ballarin.cc>
2025-03-26 17:19:02 +0100

    Update
    Signed-off-by: Emanuele Ballarin <emanuele@ballarin.cc>

--- models/getmodels.ps1
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env pwsh
-wget "https://bucket.ballarin.cc/aimldl/tsnext2024/models/RLPPO.zip"
-wget "https://bucket.ballarin.cc/aimldl/tsnext2024/models/presolved.pickle"
--- run.ps1
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env pwsh
-.\.python_env_win\Scripts\deactivate.bat
-.python_env_win\Scripts\activate.ps1
-python -O gui.py
